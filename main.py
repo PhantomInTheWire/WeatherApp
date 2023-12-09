269cef6105e41dc66914f389124364deb5b8ff4f
Karan <karanlokchandani@protonmail.com>
2023-12-09 14:27:13 +0530

    f

---
+++ main.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+import streamlit as st
+from pyowm import OWM
+import plotly.express as px
+
+# Streamlit configuration
+st.set_page_config(page_title="Weather Report App")
+
+# App title
+st.title("Weather Report App")
+
+# City input
+city = st.text_input("Enter city name", "Gandhinagar, Gujrat")
+
+key = '908ccbd2f6cca4ccbdea63188930b090'
+owm = OWM(key)
+mng = owm.weather_manager()
+weather = mng.weather_at_place(city).weather
+
+# Extract temperature data
+temperature_data = [
+    {"date": datetime.utcfromtimestamp(weather.reference_time()).isoformat()[0:10]},
+    {"temp": weather.temperature('celsius')['temp']},
+]
+
+# Create the Plotly graph
+fig = px.line(temperature_data, x="date", y="temp", title="Temperature over Time")
+
+# Create two columns
+col1, col2 = st.columns(2)
+
+# Display weather report and graph in columns
+with col1:
+    st.write(f"Date: {datetime.utcfromtimestamp(weather.reference_time()).isoformat()[0:10]}")
+    st.write(f"Temperature: {weather.temperature('celsius')['temp']}°C")
+    st.write(f"Status: {weather.detailed_status}")
+
+with col2:
+    st.write(f"Wind direction: {weather.wind()['deg']}°")
+    st.write(f"Wind: {weather.wind()['speed']} m/s")
+    st.write(f"Humidity: {weather.humidity}%")
+
+st.plotly_chart(fig)
